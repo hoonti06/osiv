261d4e31697c47e7ff44b56418456b89b1ec8265
hoonti06 <hoonti06@gmail.com>
2023-04-03 00:49:53 +0900

    first commit

---
+++ src/test/java/me/hoonti06/osiv/OsivApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoonti06.osiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsivApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
